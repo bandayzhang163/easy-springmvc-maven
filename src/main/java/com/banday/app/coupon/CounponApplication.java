18617ad40e37163de199c8a6fd0ff9a52381f56e
banday <bandayzx@gmail.com>
2021-01-12 16:16:02 +0800

    feat: 添加mybatis-plus支持，加入生成器

--- src/main/java/com/banday/app/coupon/CounponApplication.java
+++ src/main/java/com/banday/app/coupon/CounponApplication.java
@@ -1,9 +1,11 @@
-package com.banday;
+package com.banday.app.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.banday.app.coupon.mapper")
 public class CounponApplication {
     public static void main(String[] args) {
         SpringApplication.run(CounponApplication.class);
