fe8db250c42c97728ac0aa08265b1610ea03edb5
banday <bandayzx@gmail.com>
2021-01-12 15:30:38 +0800

    feat: 创建maven项目，添加springboot支持

---
+++ src/main/java/com/banday/CounponApplication.java
@@ -0,0 +1,11 @@
+package com.banday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CounponApplication.class);
+    }
+}
